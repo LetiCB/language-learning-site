6b776f00fbce0d1e42b9ae33521cc870fc971960
LetiCB <lcastrobrisson@gmail.com>
2024-12-14 22:45:25 -0300

    Create Header component

---
+++ src/components/Header/Header.styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 1.5rem;
+    height: 3.5rem;
+    background:linear-gradient(to bottom, #f9ecec, #ffffff);
+    border-bottom: 1px solid #fbdada;
+    box-shadow: 0px 4px 6px rgb(195 181 181 / 10%);
+`;
+
+export const Branding = styled.p`
+`;
+
+export const HeaderIcons = styled.div`
+`;
