f558c629b6852b900ecfeb21211bd90dbc3f166f
LetiCB <lcastrobrisson@gmail.com>
2024-12-19 22:16:34 -0300

    Update Home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,7 +16,7 @@ const HomePage: React.FC = () => {
   const handleLanguageSelect = (language: string) => {
     const languagePath = language.toLowerCase();
     setSelectedLanguage(language);
-    navigate(`/learn/${languagePath}`);
+    navigate(`/${languagePath}`);
   };
 
   return (
