e3f04f70e5d569a7e59e7859c98a50c861a1d461
LetiCB <lcastrobrisson@gmail.com>
2025-01-01 21:07:04 -0300

    Add hangman game routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,8 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import { Routes, Route } from "react-router-dom";
 import DragAndDropGame from "src/pages/DragAndDropGame";
 import DragAndDropGameList from "src/pages/DragAndDropGameListPage";
+import HangmanList from "src/pages/HangmanList";
+import HangmanPage from "src/pages/HangmanPage";
 import HomePage from "src/pages/HomePage";
 import LearnPage from "src/pages/LearnPage";
 import SongList from "src/pages/SongList";
@@ -24,6 +26,10 @@ const AppRoutes: React.FC = () => {
         <Route path="/:language/juegos-de-arrastre" element={<DragAndDropGameList />} />
 
         <Route path="/:language/juegos-de-arrastre/:title" element={<DragAndDropGame />} />
+
+        <Route path="/:language/ahoracado" element={<HangmanList />} />
+
+        <Route path="/:language/ahorcado/:theme" element={<HangmanPage />} />
 
       </Routes>
     </DndProvider>
