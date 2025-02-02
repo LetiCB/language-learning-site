078c3e17bb78229cc09eac4df7ecc499ced30366
LetiCB <lcastrobrisson@gmail.com>
2025-02-02 00:02:48 -0300

    Update notification panel component

--- src/components/NotificationPanel/NotificationPanel.styles.ts
+++ src/components/NotificationPanel/NotificationPanel.styles.ts
@@ -1,6 +1,16 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 3.125rem;
+  border-bottom: 1px solid #fbdada;
+  box-shadow: 0px 4px 6px rgb(195 181 181 / 10%);
+`;
+
 export const Panel = styled.div`
+  position: absolute;
   width: 100%;
   padding: 1rem;
   background: white;
@@ -8,6 +18,15 @@ export const Panel = styled.div`
   font-size: 1rem;
   font-weight: 500;
   color: #e91e63;
-  border-bottom: 1px solid #fbdada;
-  box-shadow: 0px 4px 6px rgb(195 181 181 / 10%);
+  transition: transform 0.5s ease-in-out, opacity 0.5s;
+
+  &.enter {
+    transform: translateX(0);
+    opacity: 1;
+  }
+
+  &.exit {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
 `;
--- src/components/NotificationPanel/NotificationPanel.tsx
+++ src/components/NotificationPanel/NotificationPanel.tsx
@@ -1,7 +1,30 @@
-import { Panel } from "./NotificationPanel.styles";
+import { useState, useEffect } from "react";
+import { Panel, Wrapper } from "./NotificationPanel.styles";
 
-const NotificationPanel = ({ message }: { message: string }) => {
-  return <Panel>{message}</Panel>;
+interface NotificationProps {
+  message: string;
+}
+
+const NotificationPanel = ({ message }: NotificationProps) => {
+  const [displayedMessage, setDisplayedMessage] = useState(message);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    if (message !== displayedMessage) {
+      setIsAnimating(true);
+
+      setTimeout(() => {
+        setDisplayedMessage(message);
+        setIsAnimating(false);
+      }, 500);
+    }
+  }, [message]);
+
+  return (
+    <Wrapper>
+      <Panel className={isAnimating ? "exit" : "enter"}>{displayedMessage}</Panel>
+    </Wrapper>
+  );
 };
-  
+
 export default NotificationPanel;
